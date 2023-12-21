e7b1cfa292656a386a98106c0c67fd1dbbf13ae4
Vasha <vasha@susana.umcc.cu>
2023-12-21 11:11:56 -0500

    Actualizando

--- vfm-admin/_content/users/users.php
+++ vfm-admin/_content/users/users.php
@@ -8,4 +8,44 @@
     'role' => 'superadmin',
     'email' => 'luis.valido@umcc.cu',
   ),
+  1 => 
+  array (
+    'name' => 'evaluador2023',
+    'pass' => '$1$MrRtbzyh$OWbZ6mIeLwF1dfc4WPgWz.',
+    'role' => 'user',
+    'disabled' => false,
+    'email' => 'evaluador2023@umcc.cu',
+  ),
+  2 => 
+  array (
+    'name' => 'responsable2023',
+    'pass' => '$1$ciZp3gkT$7L4y0oOIxBwLhg0BI0DV2/',
+    'role' => 'contributor',
+    'disabled' => false,
+    'email' => 'responsable2023@umcc.cu',
+  ),
+  3 => 
+  array (
+    'name' => 'coordinador2023',
+    'pass' => '$1$hVQ9jGvM$utbPxg.zYX7GuBuQjslMP/',
+    'role' => 'editor',
+    'email' => 'coordinador2023@umcc.cu',
+    'dir' => '["Pepe","dfg"]',
+  ),
+  4 => 
+  array (
+    'name' => 'admin2023',
+    'pass' => '$1$gaBmd3Hz$EFFMOrDaWgIfqwFBeOwZh/',
+    'role' => 'admin',
+    'disabled' => false,
+    'email' => 'admin2023@umcc.cu',
+  ),
+  5 => 
+  array (
+    'name' => 'superadmin2023',
+    'pass' => '$1$myduCFHJ$.l/36bj9iveIoMw7vadNI1',
+    'role' => 'superadmin',
+    'disabled' => false,
+    'email' => 'superadmin2023@umcc.cu',
+  ),
 );
